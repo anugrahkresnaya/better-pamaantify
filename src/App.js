fddd3ba94e3b965ef8a161d565b042d0b238e4d0
anugrahkresnaya <anugrahkresnaya@gmail.com>
2022-04-13 02:53:12 +0700

    fix component style

--- src/App.js
+++ src/App.js
@@ -1,20 +1,11 @@
 import "./App.css";
-// import data from './components/data/index';
-// import searchBar from './pages/search';
-// import Music from "./components/music";
-// import CreatePlaylist from "./components/playlist";
-// import { useState, useEffect } from "react";
 import { useSelector } from "react-redux";
-// import { setToken } from "./store/tokenSlice";
-// import url from "./components/data/Auth";
-// import axios from "axios";
 import LoginPage from "./pages/login";
 import CreatePlaylistPage from "./pages/CreatePlaylist";
 import {
   BrowserRouter as Router,
   Switch,
   Route,
-  Link,
   Redirect
 } from "react-router-dom";
 
@@ -25,17 +16,6 @@ function App() {
     <>
       <Router>
         <div>
-          <nav>
-            <ul>
-              <li>
-                <Link to="/">Login</Link>
-              </li>
-              <li>
-                <Link to="/create-playlist">Create Playlist</Link>
-              </li>
-            </ul>
-          </nav>
-
           {/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
           <Switch>
