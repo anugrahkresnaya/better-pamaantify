e6f34d3fc4120fd271e3b7b3ed1caf4a7a375854
anugrahkresnaya <anugrahkresnaya@gmail.com>
2022-06-28 10:29:59 +0700

    refactor: change redirect uri to domain

--- src/components/data/Auth.js
+++ src/components/data/Auth.js
@@ -1,5 +1,5 @@
 var client_id = process.env.REACT_APP_CLIENT_ID;
-var redirect_uri = 'http://localhost:3000/';
+var redirect_uri = 'https://better-pamaantify.vercel.app/';
 
 const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
 
